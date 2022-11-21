c6e26e11b393ecd7177d17bf26001bbd02bc2b8c
alepi <667578+alepi@users.noreply.github.com>
2022-11-21 11:08:41 +0100

    Initial Commit

---
+++ MatchesFashionTest/MatchesFashionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchesFashionTestApp.swift
+//  MatchesFashionTest
+//
+//  Created by yawmoo_ext on 21/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MatchesFashionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
